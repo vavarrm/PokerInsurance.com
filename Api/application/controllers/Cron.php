110b135bcdca6ae7bb9be70510d5ab1fbde20495
vavarrm <vavarrm@gmail.com>
2018-03-27 14:33:35 +0700

    1

--- Api/application/controllers/Cron.php
+++ Api/application/controllers/Cron.php
@@ -2,26 +2,14 @@
 defined('BASEPATH') OR exit('No direct script access allowed');
 class Cron extends CI_Controller {
 	
-	// private $request = array();
 	
-	// public function __construct() 
-	// {
-		// parent::__construct();
-    // }
+	public function __construct() 
+	{
+		parent::__construct();
+    }
 	
-	public function backupdb(){
+	public function subtotal(){
        
-	    // $this->load->dbutil();
-        // $prefs = array(
-            // 'format'      => 'text',
-            // 'filename'    => 'my_db_backup.sql'
-        // );
-        // $backup =& $this->dbutil->backup($prefs);
-        // $db_name = 'backup-on-'. date("Y-m-d-H-i-s") .'.zip';
-        // $save = './upload/_tmp/'.$db_name;
-        // $this->load->helper('file');
-        // write_file($save, $backup);
-        // $this->load->helper('download');
-        // force_download($db_name, $backup);
+	   echo "D";
     }
 }
