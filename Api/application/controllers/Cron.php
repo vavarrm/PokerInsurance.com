ce11ece5551f76aa3b2a82567657dd0227fdb35a
vavarrm <vavarrm@gmail.com>
2018-03-27 14:37:03 +0700

    1

--- Api/application/controllers/Cron.php
+++ Api/application/controllers/Cron.php
@@ -6,6 +6,14 @@ class Cron extends CI_Controller {
 	public function __construct() 
 	{
 		parent::__construct();
+		if(!empty($_SERVER['HTTP_CLIENT_IP'])){
+		   $myip = $_SERVER['HTTP_CLIENT_IP'];
+		}else if(!empty($_SERVER['HTTP_X_FORWARDED_FOR'])){
+		   $myip = $_SERVER['HTTP_X_FORWARDED_FOR'];
+		}else{
+		   $myip= $_SERVER['REMOTE_ADDR'];
+		}
+		echo $myip;
     }
 	
 	public function subtotal(){
