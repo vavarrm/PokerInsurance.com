4fd8879e21359988989070ffe4f08a0194c92150
vavarrm <vavarrm@gmail.com>
2018-03-31 20:06:49 +0700

    111

--- Api/application/models/AdminUser_Model.php
+++ Api/application/models/AdminUser_Model.php
@@ -530,7 +530,8 @@
 							INNER JOIN admin_role_permissions_link AS link ON au.ad_ar_id =  link.ar_id
 							INNER JOIN permissions AS per ON link.pe_id = per.pe_id
 						WHERE per.pe_parents_id = 0
-						ORDER BY per.pe_order_id DESC";
+						ORDER BY per.pe_order_id DESC
+						GROUP BY pe_id";
 				$query = $this->db->query($sql, $bind);
 				$error = $this->db->error();
 				if($error['message'] !="")
